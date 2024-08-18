2faa83a26df0e8992f0e55220414457481f4ae4e
srikanth05061988 <srikanth05061988@gmail.com>
2024-08-18 14:57:01 -0400

    updateds

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID for Amazon Linux 2 in us-east-1
+  ami           = "ami-04a81a99f5ec58529" # Example AMI ID for Amazon Linux 2 in us-east-1
   instance_type = "t2.micro"
   key_name      = "aws" # Replace with your key pair name
 
