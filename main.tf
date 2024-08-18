58852e2d94607797fc382d1a378b47e632fb23fb
srikanth05061988 <srikanth05061988@gmail.com>
2024-08-18 12:44:50 -0400

    first

---
+++ main.tf
@@ -0,0 +1,15 @@
+# provider.tf
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID for Amazon Linux 2 in us-east-1
+  instance_type = "t2.micro"
+  key_name      = "aws" # Replace with your key pair name
+
+  
+  tags = {
+    Name = "terraform-instance"
+  }
+}
