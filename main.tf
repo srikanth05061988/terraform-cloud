dcc24e4369db67e7e94bac3412d94ecc77f8c94d
srikanth05061988 <srikanth05061988@gmail.com>
2024-08-18 21:27:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,4 +7,8 @@ resource "aws_instance" "example" {
   ami           = "ami-04a81a99f5ec58529" # Example AMI ID for Amazon Linux 2 in us-east-1
   instance_type = "t2.micro"
   key_name      = "newkey" # Replace with your key pair name
+
+  tags {
+    "Name" = "terraform"
+  }
 }
